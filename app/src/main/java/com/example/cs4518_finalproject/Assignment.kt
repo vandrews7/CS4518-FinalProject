579b13c18c50cd965f6b027bcb462e57f1cfc529
Maranda117 <mmallen@wpi.edu>
2021-10-01 13:08:01 -0400

    assignments page layout

---
+++ app/src/main/java/com/example/cs4518_finalproject/Assignment.kt
@@ -0,0 +1,10 @@
+package com.example.cs4518_finalproject
+
+import java.util.*
+
+data class Assignment(val id: UUID = UUID.randomUUID(),
+var title: String = "",
+var subject: String = "",
+var dueDate: String = ""){
+
+}
