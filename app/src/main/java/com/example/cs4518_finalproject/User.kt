6a9626b0c3d066eae92af9661841d8ef921b4c99
vandrews7 <iamveronicaandrews@outlook.com>
2021-10-11 18:29:40 -0400

    added query to get a user's firstName to display on the welcome screen

--- app/src/main/java/com/example/cs4518_finalproject/User.kt
+++ app/src/main/java/com/example/cs4518_finalproject/User.kt
@@ -1,14 +1,10 @@
 package com.example.cs4518_finalproject
 
 import androidx.room.Entity
-import androidx.room.PrimaryKey
-import java.util.*
 
-@Entity
-class User (@PrimaryKey var id: UUID = UUID.randomUUID(),
-            var firstName: String = "",
+@Entity (primaryKeys = ["email" , "password"])
+class User (var firstName: String = "",
             var lastName: String = "",
             var email: String = "",
-            var birthday: String = "",
             var password: String = ""){
 }
--- app/src/main/java/com/example/cs4518_finalproject/database/UserDao.kt
+++ app/src/main/java/com/example/cs4518_finalproject/database/UserDao.kt
@@ -1,5 +1,6 @@
 package com.example.cs4518_finalproject.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -9,7 +10,13 @@ import com.example.cs4518_finalproject.User
 @Dao
 interface UserDao {
     @Query("Select * from user where (email=(:email) AND password=(:password))")
-    fun getUser(email: String, password: String)
+    fun login(email: String, password: String): List<User?> //maybe this is right i have no idea we will have to mess with it
+
+    @Query("Select * from user where email=(:email)") //this will be to share an assignment with another user, you just have to have their email
+    fun getUser(email: String): LiveData<List<User>>
+
+    @Query("Select firstName from user where email=(:email)")
+    fun getUsername(email: String): String
 
     @Insert
     fun addUser(user: User) //TODO: figure out how to add a user to the db user table
