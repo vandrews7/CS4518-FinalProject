4859a5318bbd0b583f83991cd6e4364d8613d509
vandrews7 <iamveronicaandrews@outlook.com>
2021-10-11 17:54:18 -0400

    added a get user query, idk if it's relevant

--- app/src/main/java/com/example/cs4518_finalproject/database/UserDao.kt
+++ app/src/main/java/com/example/cs4518_finalproject/database/UserDao.kt
@@ -2,11 +2,15 @@ package com.example.cs4518_finalproject.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 import com.example.cs4518_finalproject.User
 
 @Dao
 interface UserDao {
+    @Query("Select * from user where (email=(:email) AND password=(:password))")
+    fun getUser(email: String, password: String)
+
     @Insert
     fun addUser(user: User) //TODO: figure out how to add a user to the db user table
 
